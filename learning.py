c05376812fb2383459991d5fd8ea6e86c3f8fdbf
HrishikeshBhandarkar <hrishikeshbhandarkar6@gmail.com>
2026-06-17 20:32:16 +0530

    Random code rembering some syntaxes

---
+++ learning.py
@@ -0,0 +1,16 @@
+print("Hello World")
+for i in range (3):
+ print('OKAY')
+z=int(input("Enter a number to store in a variable and add later"))
+e=int(input('Enter another number = '))
+op=input('Do u need the sum of the both numbers?  {y/n} ')
+if op == 'y':
+ print(z+e)
+else:
+ print('OKAY')
+
+#Just writting random code to recall some most used syntaxes learned during my 11th and 12th
+
+
+
+
