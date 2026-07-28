445aac46940c6f4e1fafd50ce240bab10b1c5ee1
HrishikeshBhandarkar <hrishikeshbhandarkar6@gmail.com>
2026-07-28 19:34:51 +0530

    Add LeetCode 126 Longest sequence in an array:

---
+++ DSA-Python/Arrays/LeetCode 126 - longest possible sequence in an array .py
@@ -0,0 +1,20 @@
+def longest_sequence(array):
+    array.sort()
+    print(array)
+    last_num=array[0]
+    max_sequence=0
+    count=1
+    for i in array:
+        if last_num==i:
+            continue
+        if i == last_num+1:
+            count+=1
+        else: 
+            count=1
+        if max_sequence<count:
+            max_sequence=count
+        last_num=i
+    return  max_sequence
+array = [100, 4, 200, 1, 3, 2]
+print(array)
+print(longest_sequence(array))    
